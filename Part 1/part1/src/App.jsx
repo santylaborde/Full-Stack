93000fda3b40864587c60229e2685732493a9298
santylaborde <santylaborde@hotmail.com>
2024-03-17 15:45:05 -0300

    Arrays

--- Part 1/part1/src/App.jsx
+++ Part 1/part1/src/App.jsx
@@ -8,15 +8,18 @@ const Hello = (props) => {
 }
 
 const App = () => {
-  const name = 'Peter'
-  const age = 10
+  
+  const friends = [
+    { name: 'Peter', age: 4 },
+    { name: 'Maya', age: 10 },
+  ]
 
   return (
-    <div>
+    <>
       <h1>Greetings</h1>
-      <Hello name='Maya' age={26 + 10} />
-      <Hello name={name} age={age} />
-    </div>
+      <Hello name={friends[0].name} age={friends[0].age } />
+      <Hello name={friends[1].name} age={friends[1].age} />
+    </>
   )
 }
 
