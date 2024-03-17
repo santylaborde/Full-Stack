7d8a473befcd67296b7491437cb02bcb8c119d60
santylaborde <santylaborde@hotmail.com>
2024-03-17 15:03:58 -0300

    Multiple props

--- Part 1/part1/src/App.jsx
+++ Part 1/part1/src/App.jsx
@@ -1,18 +1,21 @@
 const Hello = (props) => {
+  console.log(props)
   return (
     <div>
-      <p>Hello {props.name}</p>
+      Hello {props.name}, you are {props.age} years old
     </div>
   )
 }
 
 const App = () => {
+  const name = 'Peter'
+  const age = 10
+
   return (
     <div>
       <h1>Greetings</h1>
-      <Hello name='George' />
-      <Hello name='Daisy' />
-      <Hello />
+      <Hello name='Maya' age={26 + 10} />
+      <Hello name={name} age={age} />
     </div>
   )
 }
