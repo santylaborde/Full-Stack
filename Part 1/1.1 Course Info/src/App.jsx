f559171ebb18681847aeab7994ec2d2ab8b6a07d
santylaborde <santylaborde@hotmail.com>
2024-03-17 23:22:55 -0300

    1.5 Done

--- Part 1/1.1 Course Info/src/App.jsx
+++ Part 1/1.1 Course Info/src/App.jsx
@@ -44,20 +44,29 @@ const Total = (props) => {
 // Root element
 const App = () => {
   // const-definitions
-  // For header
-  const course = 'Half Stack application development'
-  // For content and total
-  const parts = [
-    { name: 'Fundamentals of React', exercises: 10 },
-    { name: 'Using props to pass data', exercises: 7 },
-    { name: 'State of a component', exercises: 14 },
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <>
-      <Header course={course} />
-      <Content content={parts} />
-      <Total excercise={parts} />
+      <Header course={course.name} />
+      <Content content={course.parts} />
+      <Total excercise={course.parts} />
     </>
   )
 }
