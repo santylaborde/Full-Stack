5a723c0009b7c15c4525528b6065dc856c443114
santylaborde <santylaborde@hotmail.com>
2024-03-19 16:23:09 -0300

    State hook

--- Part 1/1.0 Lecture/02 Counter/src/App.jsx
+++ Part 1/1.0 Lecture/02 Counter/src/App.jsx
@@ -1,5 +1,23 @@
-const App = (props) => {
-  const {counter} = props
+// the file imports the useState function
+import { useState } from 'react'
+
+const App = () => {
+
+  // useState() adds state to the component and renders it initialized with the value of zero.
+  // Destructuring assignment syntax:
+  //  - "counter" -> initial value of state which is zero.
+  //  - "setCounter" -> function that will be used to modify the state.
+  const [ counter, setCounter ] = useState(0)
+
+  // setTimeout(function, timeout)
+  // e.g. The setCounter() is invoked one second after calling the setTimeout()
+  setTimeout(
+    () => setCounter(counter + 1),
+    1000
+  )
+
+  console.log('rendering...', counter)
+
   return (
     <div>{counter}</div>
   )
