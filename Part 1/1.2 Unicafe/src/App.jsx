27baf516c3319b68fcbda82f9ca10617932f7654
santylaborde <santylaborde@hotmail.com>
2024-03-20 13:24:46 -0300

    1.12 Done

--- Part 1/1.2 Unicafe/src/App.jsx
+++ Part 1/1.2 Unicafe/src/App.jsx
@@ -58,7 +58,18 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   const [all, setAll] = useState(0)
-
+  // anecdotes
+  const [selected, setSelected] = useState(0)
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
 
   // handlers
   const handleGoodClick = () => {
@@ -79,16 +90,32 @@ const App = () => {
     setAll(good + neutral + updatedBad)
   }
 
+  const handleAnecdotesClick = () => {
+    const min= 0
+    const max= anecdotes.length - 1
+    // random integer between 0 and 8
+    const updateSelected= Math.floor(Math.random() * (max - min + 1)) + min
+
+    setSelected(updateSelected)
+  }
+
   // main
   return (
     <div>
       <h1>Give feedback</h1>
       <Button handleClick={handleGoodClick} text='Good' />
       <Button handleClick={handleNeutralClick} text='Neutral' />
-      <Button handleClick={handleBadClick} text='Bad' />
+      <Button handleClick={handleBadClick} text='Bad' />    
       
-      <Statistics good={good} neutral={neutral} bad={bad} all={all} />      
+      <Statistics good={good} neutral={neutral} bad={bad} all={all} />
 
+      <h1>Give anecdote</h1>
+      {anecdotes[selected]}
+
+      <br></br><br></br>
+
+      <Button handleClick={handleAnecdotesClick} text='Next' />
+      
     </div>
   )
 }
