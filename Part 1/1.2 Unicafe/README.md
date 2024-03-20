e4a67e62e0585e4d0248ef149f6abcf008f232db
santylaborde <santylaborde@hotmail.com>
2024-03-20 13:58:27 -0300

    1.11 Done

--- Part 1/1.2 Unicafe/README.md
+++ Part 1/1.2 Unicafe/README.md
@@ -1,8 +1,5 @@
-# React + Vite
+# Unicafe
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+Like most companies, the student restaurant of the University of Helsinki Unicafe collects feedback from its customers. Your task is to implement a web application for collecting customer feedback. There are only three options for feedback: good, neutral, and bad.
 
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+The application must display the total number of collected feedback for each category.
--- Part 1/1.2 Unicafe/src/App.jsx
+++ Part 1/1.2 Unicafe/src/App.jsx
@@ -42,6 +42,7 @@ const Statistics = ({good, neutral, bad, all}) => {
             < StatisticsLine text="Good" value={good} />
             < StatisticsLine text="Neutral" value={neutral} />
             < StatisticsLine text="Bad"  value={bad} /> 
+            < StatisticsLine text="All"  value={all} /> 
             < StatisticsLine text="Average" value={ ((1)*good + (0)*neutral + (-1)*bad) / all } />
             < StatisticsLine text="Positive" value={ (good / all) * 100 + " %"} />
           </tbody>
@@ -58,20 +59,7 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   const [all, setAll] = useState(0)
-  // anecdotes
-  const [selected, setSelected] = useState(0)
-  const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-    'The only way to go fast, is to go well.'
-  ]
-    
-  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
+
 
   // handlers
   const handleGoodClick = () => {
@@ -92,41 +80,15 @@ const App = () => {
     setAll(good + neutral + updatedBad)
   }
 
-  const handleAnecdotesClick = () => {
-    const min= 0
-    const max= anecdotes.length - 1
-    // random integer between 0 and 8
-    const updateSelected= Math.floor(Math.random() * (max - min + 1)) + min
-    // update the display anecdote
-    setSelected(updateSelected)
-  }
-
-  const handleVoteClick = () => {
-    // create a copy of the matrix
-    const updatedVotes = { ...votes }
-    // add a vote to the selected anecdote
-    updatedVotes[selected] += 1
-    // update the matrix with the modified copy
-    setVotes(updatedVotes)
-  }
-
   // main
   return (
     <div>
       <h1>Give feedback</h1>
       <Button handleClick={handleGoodClick} text='Good' />
       <Button handleClick={handleNeutralClick} text='Neutral' />
-      <Button handleClick={handleBadClick} text='Bad' />    
+      <Button handleClick={handleBadClick} text='Bad' />
       
-      <Statistics good={good} neutral={neutral} bad={bad} all={all} />
-
-      <h1>Vote anecdote</h1>
-      {anecdotes[selected]}
-
-      <br></br><br></br>
-      <Button handleClick={handleVoteClick} text='Vote' />
-      <Button handleClick={handleAnecdotesClick} text='Next' />
-      <p>Has {votes[selected]} votes</p>
+      <Statistics good={good} neutral={neutral} bad={bad} all={all} />      
 
     </div>
   )
