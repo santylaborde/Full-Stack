f8c60afd5c0c1c59feafd0a6a5809bfcbe89e2b7
santylaborde <santylaborde@hotmail.com>
2024-03-19 23:10:23 -0300

    Total Clicks

--- Part 1/1.0 Lecture/03 Left Right/src/App.jsx
+++ Part 1/1.0 Lecture/03 Left Right/src/App.jsx
@@ -5,16 +5,21 @@ const App = () => {
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
   const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
 
   // handlers
   const handleLeftClick = () => {
     setAll(allClicks.concat('L'))
-    setLeft(left + 1)
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    setTotal(updatedLeft + right)
   }
 
   const handleRightClick = () => {
     setAll(allClicks.concat('R'))
-    setRight(right + 1)
+    const updatedRight = right + 1
+    setRight(updatedRight)
+    setTotal(updatedRight + right)
   }
 
   // main
@@ -26,7 +31,8 @@ const App = () => {
       <button onClick={handleLeftClick}>left</button>
       <button onClick={handleRightClick}>right</button>
       {right}
-      <p>{allClicks.join(' ')}</p>            
+      <p>{allClicks.join(' ')}</p>    
+      <p>total {total}</p>        
     </div>
   )
 }
