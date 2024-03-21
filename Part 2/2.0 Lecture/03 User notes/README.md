a121d6e579a572788fced1e5184111d302bc2b44
santylaborde <santylaborde@hotmail.com>
2024-03-21 16:50:11 -0300

    Toggle Filter

--- Part 2/2.0 Lecture/03 User notes/README.md
+++ Part 2/2.0 Lecture/03 User notes/README.md
@@ -5,3 +5,7 @@ Let's continue expanding our application by allowing users to add new notes.
 To get our page to update when new notes are added it's best to store the notes in the App component's state.
 
 Next, let's add an HTML form to the component that will be used for adding new notes.
+
+Let's add some new functionality to our application that allows us to only view the important notes.
+
+Next, let's add functionality that enables users to toggle the showAll state of the application from the user interface.
--- Part 2/2.0 Lecture/03 User notes/src/App.jsx
+++ Part 2/2.0 Lecture/03 User notes/src/App.jsx
@@ -7,7 +7,9 @@ const App = (props) => {
   // new note
   const [newNote, setNewNote] = useState(
     'a new note...'
-  ) 
+  )
+  // displayed notes
+  const [showAll, setShowAll] = useState(true)
 
   // handler button
   const addNote = (event) => {
@@ -25,21 +27,38 @@ const App = (props) => {
     // Reset the string array for new notes
     setNewNote('')
   }
-  
+
   // handler input text
   const handleNoteChange = (event) => {
     console.log(event.target.value)
     setNewNote(event.target.value)
   }
 
+  // if showAll is true, then notesToShow= notes. Else, then the filter is apply.
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important)
+
   return (
     <div>
+      {/* Title */}
       <h1>Notes</h1>
+      
+      {/* Toggle Filter */}
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all' }
+        </button>
+      </div>
+      
+      {/* Displayed Notes */}
       <ul>
-        {notes.map(note => 
+        {notesToShow.map(note =>
           <Note key={note.id} note={note} />
         )}
       </ul>
+
+      {/* New Note */}
       <form onSubmit={addNote}>
         <input value={newNote} onChange={handleNoteChange}/>
         <button type="submit">save</button>
