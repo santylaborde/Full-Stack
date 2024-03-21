fe25209bce947e5297987b754cb31d33c68cf2ba
santylaborde <santylaborde@hotmail.com>
2024-03-21 16:21:41 -0300

    New note handler

--- Part 2/2.0 Lecture/03 User notes/src/App.jsx
+++ Part 2/2.0 Lecture/03 User notes/src/App.jsx
@@ -2,7 +2,35 @@ import { useState } from 'react'
 import Note from './components/Note'
 
 const App = (props) => {
+  // stored notes
   const [notes, setNotes] = useState(props.notes)
+  // new note
+  const [newNote, setNewNote] = useState(
+    'a new note...'
+  ) 
+
+  // handler button
+  const addNote = (event) => {
+    event.preventDefault()
+
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    }
+    
+    // Add the new note
+    setNotes(notes.concat(noteObject))
+    // Reset the string array for new notes
+    setNewNote('')
+  }
+  
+  // handler input text
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
 
   return (
     <div>
@@ -12,6 +40,10 @@ const App = (props) => {
           <Note key={note.id} note={note} />
         )}
       </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange}/>
+        <button type="submit">save</button>
+      </form>   
     </div>
   )
 }
