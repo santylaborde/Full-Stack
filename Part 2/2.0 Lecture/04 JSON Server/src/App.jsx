b2a0c9299b8359607dc092752f5aba675fd3bdfc
santylaborde <santylaborde@hotmail.com>
2024-03-25 13:33:12 -0300

    Effect Hook

--- Part 2/2.0 Lecture/04 JSON Server/src/App.jsx
+++ Part 2/2.0 Lecture/04 JSON Server/src/App.jsx
@@ -1,15 +1,29 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Note from './components/Note'
 
-const App = (props) => {
+const App = () => {
   // stored notes
-  const [notes, setNotes] = useState(props.notes)
+  const [notes, setNotes] = useState([])
   // new note
   const [newNote, setNewNote] = useState(
     'a new note...'
   )
   // displayed notes
   const [showAll, setShowAll] = useState(true)
+
+  // Get notes
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }
+  
+  useEffect(hook, [])
 
   // handler button
   const addNote = (event) => {
--- Part 2/2.0 Lecture/04 JSON Server/src/main.jsx
+++ Part 2/2.0 Lecture/04 JSON Server/src/main.jsx
@@ -1,15 +1,4 @@
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import axios from 'axios'
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-axios
-  .get('http://localhost:3001/notes')
-  .then(response => {
-    const notes = response.data
-    console.log(notes)
-
-    ReactDOM.createRoot(document.getElementById('root')).render(
-      <App notes={notes} />
-    )
-  })
-
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
