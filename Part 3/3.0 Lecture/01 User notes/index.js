c1fa2f5daf4e74fd200b5b6032ab4e5099c6b2df
santylaborde <santylaborde@hotmail.com>
2024-04-02 11:24:29 -0300

    post new note

--- Part 3/3.0 Lecture/01 User notes/index.js
+++ Part 3/3.0 Lecture/01 User notes/index.js
@@ -19,14 +19,20 @@ let notes = [
   }
 ]
 
+// json-parser
+app.use(express.json())
+
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
 
+/*** API ***/
+// get all
 app.get('/api/notes', (request, response) => {
     response.json(notes)
 })
 
+// get singular
 app.get('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id)
     const note = notes.find(note => note.id === id)
@@ -38,6 +44,7 @@ app.get('/api/notes/:id', (request, response) => {
     }
 })
 
+// delete
 app.delete('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id)
     notes = notes.filter(note => note.id !== id)
@@ -45,6 +52,21 @@ app.delete('/api/notes/:id', (request, response) => {
     response.status(204).end()
 })
 
+// create
+app.post('/api/notes', (request, response) => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id)) 
+    : 0
+
+  const note = request.body
+  note.id = maxId + 1
+
+  notes = notes.concat(note)
+
+  response.json(note)
+})
+
+/*** MAIN ***/
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
