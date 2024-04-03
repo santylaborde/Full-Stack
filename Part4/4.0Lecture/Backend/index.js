eb7918a62bec8f5f5ce90d116b21d1c65458cf7d
santylaborde <santylaborde@hotmail.com>
2024-04-03 20:16:32 -0300

    test

---
+++ Part4/4.0Lecture/Backend/index.js
@@ -0,0 +1,105 @@
+const express = require('express')
+const app = express()
+
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    important: true
+  },
+  {
+    id: 2,
+    content: "Browser can execute only JavaScript",
+    important: false
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true
+  }
+]
+
+/*** MIDDLEWARES ***/
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('Status:  ', request.status)
+  console.log(request);
+  console.log('---')
+  next()
+}
+
+const cors = require('cors')
+app.use(cors())
+
+// json-parser
+app.use(express.json())
+// request info
+app.use(requestLogger)
+
+/*** FUNCTIONS ***/
+// id generator
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+})
+
+/*** API ***/
+// get all
+app.get('/api/notes', (request, response) => {
+    response.json(notes)
+})
+
+// get singular
+app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const note = notes.find(note => note.id === id)
+    
+    if (note) {
+      response.json(note)
+    } else {
+      response.status(404).end()
+    }
+})
+
+// delete
+app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+  
+    response.status(204).end()
+})
+
+// create
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: Boolean(body.important) || false,
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+
+  response.json(note)
+})
+
+/*** MAIN ***/
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
