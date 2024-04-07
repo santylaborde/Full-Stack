3e2388402fc36467d12d6b14b1b5927768a51d10
santylaborde <santylaborde@hotmail.com>
2024-04-07 13:32:15 -0300

    SAVE to DB

--- Part 3/3.0 Lecture/01 User notes/Backend/index.js
+++ Part 3/3.0 Lecture/01 User notes/Backend/index.js
@@ -70,21 +70,22 @@ app.delete('/api/notes/:id', (request, response) => {
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
+  // not empty
   if (!body.content) {
     return response.status(400).json({ 
       error: 'content missing' 
     })
   }
 
-  const note = {
+  // new note
+  const note = new Note({
     content: body.content,
-    important: Boolean(body.important) || false,
-    id: generateId(),
-  }
+    important: body.important || false,
+  })
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
 /*** MAIN ***/
