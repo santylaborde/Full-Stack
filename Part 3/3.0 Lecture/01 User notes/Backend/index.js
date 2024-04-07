11722670868410541ee07d1911ada8d72f12fc8f
santylaborde <santylaborde@hotmail.com>
2024-04-07 13:36:38 -0300

    findById DB

--- Part 3/3.0 Lecture/01 User notes/Backend/index.js
+++ Part 3/3.0 Lecture/01 User notes/Backend/index.js
@@ -48,14 +48,9 @@ app.get('/api/notes', (request, response) => {
 
 // get singular
 app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-    
-    if (note) {
-      response.json(note)
-    } else {
-      response.status(404).end()
-    }
+  Note.findById(request.params.id).then(note => {
+    response.json(note)
+  })
 })
 
 // delete
