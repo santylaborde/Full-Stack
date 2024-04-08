130e1ebf54b610381c9b609b8f52e51e4c74ed45
santylaborde <santylaborde@hotmail.com>
2024-04-08 19:23:59 -0300

    Update Note Validation

--- Part 3/3.0 Lecture/01 User notes/Backend/index.js
+++ Part 3/3.0 Lecture/01 User notes/Backend/index.js
@@ -97,15 +97,12 @@ app.post('/api/notes', (request, response, next) => {
 
 // update
 app.put('/api/notes/:id', (request, response, next) => {
-  const body = request.body
+  const { content, important } = request.body
 
-  // a regular JavaScript object
-  const note = {
-    content: body.content,
-    important: body.important,
-  }
-
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+  Note.findByIdAndUpdate(
+    request.params.id, 
+    { content, important }, 
+    { new: true, runValidators: true, context: 'query' })
     .then(updatedNote => {
       response.json(updatedNote)
     })
